634a8fa278c6495c1f873c3ef69c3c456d84b91c
Al Aqmar <alaqmarak0810@gmail.com>
2026-07-24 23:21:44 +0530

    chore: remove default vercel favicon and configure new favicons in metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,7 +34,17 @@ export const metadata: Metadata = {
     siteName: "Classic Edge 53",
     locale: "en_US",
     type: "website",
-  }
+  },
+  icons: {
+    icon: [
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+    ],
+    apple: [
+      { url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' },
+    ],
+  },
+  manifest: '/site.webmanifest',
 };
 
 export default async function RootLayout({
